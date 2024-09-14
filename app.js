047273a770323c2e4d81b6f8dbdd853c969e2d53
josMuiruri <josemuiruri51@gmail.com>
2024-09-14 20:41:14 +0300

    Handling the unhandled routes

--- app.js
+++ app.js
@@ -19,4 +19,12 @@ app.use(express.json());
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/users', userRouter);
 
+// handling unhandled Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can not find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
