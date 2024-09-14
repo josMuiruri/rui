8af9744839918243efbbd84dc1c7fbe2972a7b56
josMuiruri <josemuiruri51@gmail.com>
2024-09-14 22:10:11 +0300

    Error class creation & refactoring - appError,errorController

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -21,25 +23,9 @@ app.use('/api/v1/users', userRouter);
 
 // handling unhandled Routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `can not find ${req.originalUrl} on this server!`,
-  // });
-  const err = new Error(`can not find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`can not find ${req.originalUrl} on this server!`, 404));
 });
 
 // err handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 module.exports = app;
