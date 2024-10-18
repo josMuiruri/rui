3b2495ee9bc2c6083b42402bba09a403b4191cba
josMuiruri <josemuiruri51@gmail.com>
2024-10-18 17:16:57 +0300

    loging in Users with an API

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-// overview
+router.use('/', authController.isLoggedIn);
+
 router.get('/', viewsController.getOverview);
 router.get('/product/:slug', viewsController.getProduct);
 router.get('/login', viewsController.getLoginForm);
