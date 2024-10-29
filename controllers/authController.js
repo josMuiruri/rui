f44513a0541efa3638515ca10fdd201d665d4ea5
josMuiruri <josemuiruri51@gmail.com>
2024-10-29 03:32:48 +0300

    using sendgrid

--- controllers/authController.js
+++ controllers/authController.js
@@ -184,16 +184,9 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // Send it to user's email
-  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't request to change password, Please ignore this email.`;
-
   try {
-    // await Email({
-    //   email: user.email,
-    //   subject: 'Your password reset token is valid for 10 minutes',
-    //   message,
-    // });
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
